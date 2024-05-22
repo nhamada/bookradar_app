ebc92a12e23a4ccdb766a12f1b8dd1b5e9a3e5c3
Naohiro Hamada <nao.pinguinus@icloud.com>
2024-05-23 00:13:25 +0900

    Add decodable default implementation

--- Sources/BookRadarEntity/GoogleBooks/GBAVolume.swift
+++ Sources/BookRadarEntity/GoogleBooks/GBAVolume.swift
@@ -13,4 +13,21 @@ public struct GBAVolume: Decodable {
     public let etag: String
     public let volumeInfo: GBAVolumeInfo
     public let saleInfo: GBAVolumeSaleInfo
+    
+    public enum CodingKeys: CodingKey {
+        case kind
+        case id
+        case etag
+        case volumeInfo
+        case saleInfo
+    }
+    
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.kind = try container.decode(String.self, forKey: .kind)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.etag = try container.decode(String.self, forKey: .etag)
+        self.volumeInfo = try container.decode(GBAVolumeInfo.self, forKey: .volumeInfo)
+        self.saleInfo = try container.decode(GBAVolumeSaleInfo.self, forKey: .saleInfo)
+    }
 }
