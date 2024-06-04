71a31ca41da361a5bb9f28a048f8f9aeb4d82602
Naohiro Hamada <nao.pinguinus@icloud.com>
2024-06-04 21:12:19 +0900

    Update package
    APIクライアントのパッケージを追加

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "bookradar_app",
+    platforms: [
+        .iOS(.v17)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -22,6 +25,9 @@ let package = Package(
         ),
         .target(
             name: "BookRadarEntity"
+        ),
+        .target(
+            name: "APIClient"
         ),
         .testTarget(
             name: "BookRadarAppTests",
