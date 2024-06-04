9d5fee723c869795aedefe1acb3dcba54e13c843
Naohiro Hamada <nao.pinguinus@icloud.com>
2024-06-04 23:07:43 +0900

    APIリクエストの定義

---
+++ Sources/APIClient/APIRequest.swift
@@ -0,0 +1,47 @@
+//
+//  APIRequest.swift
+//
+//
+//  Created by Naohiro Hamada on 2024/05/29.
+//
+
+import Foundation
+
+public protocol APIRequest {
+    var cachePolicy: URLRequest.CachePolicy { get }
+    var timeoutInterval: TimeInterval { get }
+    
+    var scheme: APIHTTPScheme { get }
+    var hostname: String { get }
+    var path: String { get }
+    
+    var method: APIHTTPMethod { get }
+    
+    var queryParameter: APIRequestQueryParameter? { get }
+    var body: APIRequestBodyParameter? { get }
+    
+    func build() -> URLRequest
+}
+
+extension APIRequest {
+    func build() -> URLRequest {
+        var urlComponent = URLComponents()
+        
+        urlComponent.scheme = self.scheme.stringValue
+        urlComponent.host = self.hostname
+        urlComponent.path = self.path
+        urlComponent.queryItems = self.queryParameter?.items
+        
+        guard let url = urlComponent.url else {
+            fatalError()
+        }
+        
+        var request = URLRequest(url: url,
+                                 cachePolicy: self.cachePolicy,
+                                 timeoutInterval: self.timeoutInterval)
+        request.httpMethod = self.method.stringValue
+        request.httpBody = self.body?.data
+        
+        return request
+    }
+}
