8586af60a0539afcc9cd5d79fbad3e459cb0e98c
Naohiro Hamada <nao.pinguinus@icloud.com>
2024-06-17 00:13:36 +0900

    Update search implementation
    実装したパラメーターを用いて、検索する処理を実装

--- Sources/GoogleBooksRepository/GoogleBooksRepository.swift
+++ Sources/GoogleBooksRepository/GoogleBooksRepository.swift
@@ -18,8 +18,21 @@ public final class GoogleBooksRepository {
         self.apiClient = apiClient
     }
     
-    public func search(title: String) async -> Result<[GBAVolume], APIClientError> {
-        let request = VolumeSearchRequest(queryParameter: .init(query: "intitle:\(title)"))
+    public func search(subject: GoogleBookSearchSubject,
+                       page: GoogleBookSearchPage? = nil,
+                       order: GoogleBookSearchOrder? = nil) async -> Result<[GBAVolume], APIClientError> {
+        await search(subjects: [subject],
+                     page: page,
+                     order: order)
+    }
+    
+    public func search(subjects: [GoogleBookSearchSubject],
+                       page: GoogleBookSearchPage? = nil,
+                       order: GoogleBookSearchOrder? = nil) async -> Result<[GBAVolume], APIClientError> {
+        let requestParameter = VolumeSearchRequestParameter(subjects: subjects,
+                                                            page: page,
+                                                            order: order)
+        let request = VolumeSearchRequest(queryParameter: requestParameter)
         let result = await apiClient.send(request: request)
         switch result {
         case .success(let success):
